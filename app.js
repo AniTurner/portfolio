bb52b1a7d61750ae7eaebc5e469f42b4d19b339c
Ani Turner <aniturner93@gmail.com>
2019-04-11 14:45:13 -0600

    added portfolio outline

--- app.js
+++ app.js
@@ -4,3 +4,13 @@ let navBarToggle = document.getElementById('js-navbar-toggle');
 navBarToggle.addEventListener('click', function () {
   mainNav.classList.toggle('active');
 });
+
+
+// CONTACT
+<script language="JavaScript">
+var frmvalidator  = new Validator("contactform");
+frmvalidator.addValidation("name","req","Please provide your name");
+frmvalidator.addValidation("email","req","Please provide your email");
+frmvalidator.addValidation("email","email",
+  "Please enter a valid email address");
+</script>
